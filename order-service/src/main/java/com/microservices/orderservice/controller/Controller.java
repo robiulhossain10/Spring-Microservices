4cf80d2954e10ce10a96ffd696fce614ad3fabe8
Robiul Hossain <robiulhossain0110@gmail.com>
2025-09-27 20:28:43 +0600

    add new

--- order-service/src/main/java/com/microservices/orderservice/controller/Controller.java
+++ order-service/src/main/java/com/microservices/orderservice/controller/Controller.java
@@ -1,14 +1,14 @@
 package com.microservices.orderservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/order/delivary")
-class deliveryController {
+@RequestMapping("/order")
+public class Controller {
     @GetMapping
-    public String getUsers() {
-        return "User list from Delivery-SERVICE";
+    public String get(){
+        return  "Order Service Controller";
     }
 }
